16c66fdde2c58c26d577e73e0a633e0431e10b98
AnantMishra121 <anantmishra727@gmail.com>
2026-07-29 23:13:57 +0530

    refactor user model to use array for cart and implement add to cart api and fetch cart

--- model/usermodel.js
+++ model/usermodel.js
@@ -4,10 +4,12 @@ const UserSchema = mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  cart: {
-    type: Array,
-    default: [],
-  },
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "product",
+    },
+  ],
 
   orders: {
     type: Array,
