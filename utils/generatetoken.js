6d4f70789c4020fb956d2e647829dd0d163cdca5
AnantMishra121 <anantmishra727@gmail.com>
2026-08-04 15:24:48 +0530

    refactor token generation to include expiration time

--- utils/generatetoken.js
+++ utils/generatetoken.js
@@ -1,20 +1,27 @@
 const jwt = require("jsonwebtoken");
 
+const ExpireIN = "7d";
+
 const generatetoken = (user) => {
-  return jwt.sign({ email: user.email, id: user._id }, process.env.JWT_KEY);
+  return (
+    jwt.sign({ email: user.email, id: user._id }, process.env.JWT_KEY),
+    { expiresIn: ExpireIN }
+  );
 };
 
 const generateOwnerToken = (owner) => {
-  return jwt.sign(
-    {
-      email: owner.email,
-      id: owner._id,
-      role: "owner",
-    },
-    process.env.JWT_KEY,
+  return (
+    jwt.sign(
+      {
+        email: owner.email,
+        id: owner._id,
+        role: "owner",
+      },
+      process.env.JWT_KEY,
+    ),
+    { expiresIn: ExpireIN }
   );
 };
 
 module.exports.generatetoken = generatetoken;
 module.exports.generateOwnerToken = generateOwnerToken;
-
